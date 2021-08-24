065489481af1cde99aca4220c335e3d1a32dd95d
ishan <sangalishan1234@gmail.com>
2021-08-24 15:36:21 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const http=require("http")
+const fetch=require("node-fetch")
+const jsdom = require("jsdom");
+const fs=require("fs")
+const { JSDOM } = jsdom;
+global.document = new JSDOM().window.document;
+let htmlfile=fs.readFileSync("index.html","utf-8")
+const replace=(tempvalue,newvalue)=>{
+    // let temp=tempvalue.replace("{%title%}",newvalue.title)
+    // temp=temp.replace("{%desc%}",newvalue.body)
+    let temp=tempvalue.replace("{%divappend%}",`${newvalue}`)
+    return temp;
+
+
+}
+
+
+
+
+
+
+const server=http.createServer((req,res)=>{
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then((json) =>{
+
+  let div=document.createElement("div")
+    json.forEach((element) => {
+
+if(div.innerHTML)
+{
+  div.innerHTML+=`<div class="card mid min-width" style="width: 18rem;" >
+  <div>
+  
+  
+  <img src="https://source.unsplash.com/random" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${element.title}</h5>
+    <p class="card-text">${element.body}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>`
+}
+else{
+// console.log(element.title)
+  div.innerHTML=`<div class="card mid min-width" style="width: 18rem;" >
+  <div>
+  
+  
+  <img src="https://source.unsplash.com/random" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${element.title}</h5>
+    <p class="card-text">${element.body}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>`
+}
+
+
+        // console.log(element[0])
+        
+      })
+      const newhtmlfile= replace(htmlfile,div.innerHTML)
+      
+      res.end(newhtmlfile)
+             
+    })
+    
+} )
+ 
+       
+server.listen(3000,"127.0.0.1")
+
